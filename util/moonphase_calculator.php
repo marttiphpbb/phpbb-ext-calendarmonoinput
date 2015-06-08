2892dc1c2908de6a3f529f8e14870d95b230f87c
martti <info@martti.be>
2015-06-08 08:37:53 +0200

    cleanup

--- util/moonphase_calculator.php
+++ util/moonphase_calculator.php
@@ -1,38 +1,16 @@
 <?php
 /**
-<<<<<<< HEAD
-<<<<<<< HEAD
-* phpBB Extension - marttiphpbb calendar
-* @copyright (c) 2014 marttiphpbb <info@martti.be>
-* @license GNU General Public License, version 2 (GPL-2.0)
-* adapted from Stephen A. Zarkos, Calculate Moon Phase Data with PHP
-* http://www.obsid.org/2008/05/calculate-moon-phase-data-with-php.html
-=======
-=======
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
 * @package phpBB Extension - marttiphpbb calendar
 * @copyright (c) 2014 marttiphpbb <info@martti.be>
 * @license http://opensource.org/licenses/MIT
 * adapted from Stephen A. Zarkos, Calculate Moon Phase Data with PHP
 * http://www.obsid.org/2008/05/calculate-moon-phase-data-with-php.html 
-<<<<<<< HEAD
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
-=======
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
 */
 
 namespace marttiphpbb\calendar\util;
 
 use marttiphpbb\calendar\core\timespan;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
-=======
-
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
 class moonphase_calculator
 {
 
@@ -41,110 +19,47 @@ class moonphase_calculator
 		1	=> 'q1',
 		2	=> 'full',
 		3	=> 'q3',
-<<<<<<< HEAD
-<<<<<<< HEAD
 	);
-=======
-	);	
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
-=======
-	);	
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
 
 	public function __construct()
 	{
 	}
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 	/*
 	* Find phases of the moon between two unix times.
-	* phases
-	*    0: new moon
-	*    1: first quarter
-	*    2: full moon
-=======
-=======
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
-	
-	/*
-	* Find phases of the moon between two unix times.
-	* phases 
-	*    0: new moon
-	*    1: first quarter 
-	*    2: full moon 
-<<<<<<< HEAD
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
-=======
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
-	*    3: third quarter
 	* @param 	timespan $timespan
 	* @return 	array
 	* 				phase 	0: new moon, 1: first quarter, 2: full moon, 3: 3th quarter
 	* 				time  	unix time
 	* 				name 	new q1 full q3
 	*/
-<<<<<<< HEAD
-<<<<<<< HEAD
-	public function find_in_timespan(timespan $timespan)
-	{
-		$phases = array();
-
-		// Convert to Julian time
-		$start_jd = ($timespan->get_start() / 86400) + 2440587.5;
-		$end_jd = ($timespan->get_end() / 86400) + 2440587.5;
-
-		$synodic_month_length = 29.53058868;
-		$deg = pi() / 180;
-		$max_moon_cycles = 100;
-
-		$day_floor = $start_jd;
-
-		if ($day_floor >= 2299161)
-=======
-=======
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
 	public function find_in_timespan(timespan $timespan)  
 	{
 		$phases = array();
-		
+
 		// Convert to Julian time
 		$start_jd = ($timespan->get_start() / 86400) + 2440587.5;
 		$end_jd = ($timespan->get_end() / 86400) + 2440587.5;
-		
+
 		$synodic_month_length = 29.53058868;	
 		$deg = pi() / 180;
 		$max_moon_cycles = 100; 
 
 		$day_floor = $start_jd;
-		
+
 		if ($day_floor >= 2299161)  
-<<<<<<< HEAD
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
-=======
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
 		{
 			$alpha = floor(($day_floor - 1867216.25) / 36524.25);
 			$day_floor = $day_floor + 1 + $alpha - floor($alpha / 4);
 		}
 
 		$day_floor += 1524;
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
-		
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
-=======
-		
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
 		$year = floor(($day_floor - 122.1) / 365.25);
 		$month = floor(($day_floor - floor(365.25 * $year)) / 30.6001);
 
 		$month = ($month < 14) ? $month - 1 : $month - 13;
 		$year = ($month > 2) ? $year - 4716 : $year - 4715;
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 		$syn_month = floor(($year + (($month - 1) * (1 / 12)) - 1900) * 12.3685) - 2;  // before :: -2
 
@@ -152,20 +67,6 @@ class moonphase_calculator
 		{
 			$phase = $syn_month - floor($syn_month);
 
-=======
-=======
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
-		
-		$syn_month = floor(($year + (($month - 1) * (1 / 12)) - 1900) * 12.3685) - 2;  // before :: -2
-	
-		for ($max_loop = $syn_month + $max_moon_cycles; $syn_month < $max_loop; $syn_month += 0.25)
-		{		
-			$phase = $syn_month - floor($syn_month);
-				
-<<<<<<< HEAD
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
-=======
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
 			$jc_time = $syn_month / 1236.85;		// time in Julian centuries from 1900 January 0.5
 			$jc_time2 = $jc_time * $jc_time;		// square for frequent use
 			$jc_time3 = $jc_time2 * $jc_time;		// cube for frequent use
@@ -195,15 +96,7 @@ class moonphase_calculator
 				- 0.0016528 * $jc_time2
 				- 0.00000239 * $jc_time3;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-			if (($phase < 0.01) || (abs($phase - 0.5) < 0.01))
-=======
 			if (($phase < 0.01) || (abs($phase - 0.5) < 0.01))  
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
-=======
-			if (($phase < 0.01) || (abs($phase - 0.5) < 0.01))  
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
 			{
 				// Corrections for New and Full Moon.
 				$phase_time += (0.1734 - 0.000393 * $jc_time) * sin($sun_anom * $deg)
@@ -220,15 +113,7 @@ class moonphase_calculator
 					+ 0.0010 * sin((2 * $moon_lat - $moon_anom) * $deg)
 					+ 0.0005 * sin(($sun_anom + 2 * $moon_anom) * $deg);
 			}
-<<<<<<< HEAD
-<<<<<<< HEAD
-			else if ((abs($phase - 0.25) < 0.01 || (abs($phase - 0.75) < 0.01)))
-=======
 			else if ((abs($phase - 0.25) < 0.01 || (abs($phase - 0.75) < 0.01)))  
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
-=======
-			else if ((abs($phase - 0.25) < 0.01 || (abs($phase - 0.75) < 0.01)))  
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
 			{
 				$phase_time += (0.1721 - 0.0004 * $jc_time) * sin($sun_anom * $deg)
 					+ 0.0021 * sin(2 * $sun_anom * $deg)
@@ -245,53 +130,19 @@ class moonphase_calculator
 					+ 0.0003 * sin(($sun_anom + 2 * $moon_anom) * $deg)
 					+ 0.0004 * sin(($sun_anom - 2 * $moon_anom) * $deg)
 					- 0.0003 * sin((2 * $sun_anom + $moon_anom) * $deg);
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-				if ($phase < 0.5)
-=======
-					
 				if ($phase < 0.5)  
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
-=======
-					
-				if ($phase < 0.5)  
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
 				{
 					// First quarter correction.
 					$phase_time += 0.0028 - (0.0004 * cos($sun_anom * $deg)) + (0.0003 * cos($moon_anom * $deg));
 				}
-<<<<<<< HEAD
-<<<<<<< HEAD
 				else
-=======
-				else 
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
-=======
-				else 
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
 				{
 					// Last quarter correction.
 					$phase_time += -0.0028 + (0.0004 * cos($sun_anom * $deg)) - (0.0003 * cos($moon_anom * $deg));
 				}
-<<<<<<< HEAD
-<<<<<<< HEAD
 			}
 
-			if ($phase_time >= $end_jd)
-			{
-				return $phases;
-			}
-
-			if ($phase_time >= $start_jd)
-			{
-				$phase = (int) floor(4 * $phase);
-
-=======
-=======
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
-			}		
-			
 			if ($phase_time >= $end_jd)  
 			{
 				return $phases;
@@ -300,29 +151,13 @@ class moonphase_calculator
 			if ($phase_time >= $start_jd)  
 			{
 				$phase = (int) floor(4 * $phase);
-				
-<<<<<<< HEAD
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
-=======
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
+
 				$phases[] = array(
 					'phase'	=> $phase,
 					'time'	=> round(($phase_time - 2440587.5) * 86400),
 					'name'	=> moonphase_calculator::$moon_name_ary[$phase],
 				);
-<<<<<<< HEAD
-<<<<<<< HEAD
 			}
 		}
 	}
-=======
-			}		
-		} 
-	}	
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
-=======
-			}		
-		} 
-	}	
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
 }
--- util/timeformat.php
+++ util/timeformat.php
@@ -1,20 +1,8 @@
 <?php
 /**
-<<<<<<< HEAD
-<<<<<<< HEAD
-* phpBB Extension - marttiphpbb calendar
-* @copyright (c) 2014 marttiphpbb <info@martti.be>
-* @license GNU General Public License, version 2 (GPL-2.0)
-=======
 * @package phpBB Extension - marttiphpbb calendar
 * @copyright (c) 2014 marttiphpbb <info@martti.be>
 * @license http://opensource.org/licenses/MIT
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
-=======
-* @package phpBB Extension - marttiphpbb calendar
-* @copyright (c) 2014 marttiphpbb <info@martti.be>
-* @license http://opensource.org/licenses/MIT
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
 */
 
 namespace marttiphpbb\calendar\util;
@@ -23,79 +11,37 @@ use phpbb\user;
 
 /*
  * derives a timeformat from the user's dateformat.
-<<<<<<< HEAD
-<<<<<<< HEAD
  */
 
 class timeformat
 {
 
-	/*
-	 * @var string
-	 */
-
+	/* @var string */
 	protected $format;
 
 	const DEFAULT_FORMAT = 'g:i a';
 
+	/* @var array */
 	protected static $format_candidates = array(
 		'H:i', 'H\hi', 'g:i a', 'g\hi a',
 	);
 
-	/*
-	 * @param user $user
-	 */
-=======
-=======
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
- */ 
+	/* @var user */
+	protected $user;
 
-class timeformat
-{	
-	
-	/*
-	 * @var string
-	 */
-	 
-	protected $format;
-	
-	const DEFAULT_FORMAT = 'g:i a';
-	
-	protected static $format_candidates = array(
-		'H:i', 'H\hi', 'g:i a', 'g\hi a',
-	);
-	
 	/*
 	 * @param user $user
 	 */ 
-<<<<<<< HEAD
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
-=======
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
 	public function __construct(
 		user $user
 	)
 	{
 		$this->user = $user;
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 		$dateformat = $user->data['user_dateformat'];
 
 		$this->format = timeformat::DEFAULT_FORMAT;
 
-=======
-=======
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
-		
-		$dateformat = $user->data['user_dateformat'];
-		
-		$this->format = timeformat::DEFAULT_FORMAT;
-		
-<<<<<<< HEAD
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
-=======
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
 		foreach (timeformat::$format_candidates as $format)
 		{
 			if (strpos($dateformat, $format) === 0 || strpos($dateformat, ' ' . $format))
@@ -105,19 +51,10 @@ class timeformat
 			}
 		}
 	}
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
-=======
-	
->>>>>>> 8687857353fbed65fbb9e283a9ed0ee69023d194
 	/*
 	 * @return string
-	 */
-
+	*/
 	public function __toString()
 	{
 		return $this->format;
