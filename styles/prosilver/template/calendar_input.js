2cf8dd709ef6a3d77037410eebc4ecefe7fa36da
martti <info@martti.be>
2016-02-07 21:43:18 +0100

    repair datepicker

--- styles/prosilver/template/calendar_input.js
+++ styles/prosilver/template/calendar_input.js
@@ -1,19 +1,18 @@
 ;(function($, window, document) {
-
 	$('document').ready(function(){
+		var $ci = $('div.calendar-input').eq(0);
 
 		$('#calendar_date_start').datepicker({
 			dateFormat: "yy-mm-dd",
-			minDate: calendarMinDate,
-			maxDate: calendarMaxDate
+			minDate: $ci.data('min-date'),
+			maxDate: $ci.data('max-date')
 		});
 
 		$('#calendar_date_end').datepicker({
 			dateFormat: "yy-mm-dd",
-			minDate: calendarMinDate,
-			maxDate: calendarMaxDate + calendarMaxLength
+			minDate: $ci.data('min-date'),
+			maxDate: $ci.data('max-date') + $ci.data('max-length')
 		});
-		
-	});
 
+	});
 })(jQuery, window, document);
