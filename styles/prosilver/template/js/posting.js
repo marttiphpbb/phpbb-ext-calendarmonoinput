eed455ea59758ac44f6cf21ee6f03daed199f249
martti <info@martti.be>
2018-07-23 20:47:34 +0200

    tabindex

--- styles/prosilver/template/js/posting.js
+++ styles/prosilver/template/js/posting.js
@@ -3,26 +3,39 @@
 		var $ci = $('div.marttiphpbb-calendarinput');
 		var $start = $ci.find('input[data-marttiphpbb-calendarinput="start"]');
 		var $end = $ci.find('input[data-marttiphpbb-calendarinput="end"]');
-
 		var data = $ci.data('marttiphpbb-calendarinput');
-
-		console.log(data.minLimit);
-		console.log(data.maxLimit);
 
 		var startPicker = $start.datepicker({
 			altField: "#alt_" + $start.attr('id'),
 			altFormat: "yy-mm-dd",
-			dateFormat: "yy-mm-dd",
+			dateFormat: data.dateFormat,
 			minDate: data.minLimit,
-			maxDate: data.maxLimit
+			maxDate: data.maxLimit,
+			onSelect: function(textDate, inst){
+				startSelect(textDate, inst);
+			}
 		});
 
 		var endPicker = $end.datepicker({
 			altField: "#alt_" + $end.attr('id'),
 			altFormat: "yy-mm-dd",
-			dateFormat: "yy-mm-dd",
+			dateFormat: data.dateFormat,
 			minDate: data.minLimit,
-			maxDate: data.maxLimit + data.maxDuration
+			maxDate: data.maxLimit + data.maxDuration,
+			onSelect: function(textDate, inst){
+				endSelect(textDate, inst);
+			}
 		});
+
+		function startSelect(dateText, inst){
+			console.log('start');
+			console.log(dateText);
+			console.log(inst);
+		}
+
+		function endSelect(dateText, inst){
+			console.log('end');
+			console.log(dateText);
+		}
 	});
 })(jQuery, window, document);
